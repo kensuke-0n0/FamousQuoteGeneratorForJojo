d0058b3d0f74db2f28c4a12a9df85c93124e8fa3
Kensuke <kensuke.n9925@gmail.com>
2024-05-13 00:07:57 +0900

    Initial Commit

---
+++ FamousQuoteGeneratorForJojoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FamousQuoteGeneratorForJojoApp
+//
+//  Created by にしいけんすけ on 2024/05/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
