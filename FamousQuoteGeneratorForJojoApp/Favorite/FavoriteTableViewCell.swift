a7c1b0d490d9c7669079c24c8eb93df775f57d87
Kensuke <kensuke.n9925@gmail.com>
2024-06-12 21:50:05 +0900

    tableViewの実装

---
+++ FamousQuoteGeneratorForJojoApp/Favorite/FavoriteTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FavoriteTableViewCell.swift
+//  FamousQuoteGeneratorForJojoApp
+//
+//  Created by にしいけんすけ on 2024/06/12.
+//
+
+import UIKit
+
+class FavoriteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- FamousQuoteGeneratorForJojoApp/FavoriteViewController.swift
+++ FamousQuoteGeneratorForJojoApp/FavoriteViewController.swift
@@ -9,22 +9,53 @@ import UIKit
 
 /// お気に入り画面
 class FavoriteViewController: UIViewController {
-
+    
+    // MARK: - Outlets
+    
+    @IBOutlet private weak var tableView: UITableView!
+    
+    // MARK: - View Life-Cycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureTableView()
+        
+        navigationItem.title = "お気に入り"
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: - Other Methods
+    
+    private func configureTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        // カスタムセル
+        let nib = UINib(nibName: "FavoriteTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "Cell")
     }
-    */
+}
 
+// MARK: - UITableViewDataSource
+
+extension FavoriteViewController: UITableViewDataSource {
+    /// データの数（＝セルの数）を返すメソッド
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    /// 各セルの内容を返すメソッド
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 再利用可能な cell を得る
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)as! FavoriteTableViewCell
+        // ここにセルに渡す処理を書く
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension FavoriteViewController: UITableViewDelegate {
+    /// セルの高さを設定するメソッド
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
 }
